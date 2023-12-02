ec1e163004e9e9ea0fd6c1017f3f813cd8570339
Marcelo Quadrilatero <marceloquadrilatero@gmail.com>
2023-12-01 22:04:41 -0300

    Ecomerce

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const menuDesplegable       = document.querySelector('.ul-menu');
-const botonMenu             = document.querySelector('.icono-menu');
-const botonBuscar           = document.querySelector('.icono-buscar');
-const botonBack             = document.querySelector('.flecha-back');
-const buscadorDesplegable   = document.querySelector('.buscador');
-const titulo                = document.querySelector('.titulo');
+const menuDesplegable           = document.querySelector('.ul-menu');
+const botonMenu                 = document.querySelector('.icono-menu');
+const botonBuscar               = document.querySelector('.icono-buscar');
+const botonBack                 = document.querySelector('.flecha-back');
+const buscadorDesplegable       = document.querySelector('.buscador');
+const titulo                    = document.querySelector('.titulo');
+const botonBackCarrito         = document.querySelector('.flecha-back-seccion-carrito');
+const botonCarritoShop          = document.querySelector('.icono-shop')
+const seccionCarritoDesplegable = document.querySelector('.contenedor-seccion-carrito')
 
 
 botonMenu.addEventListener('click',()=>{
@@ -29,5 +32,12 @@ botonBack.addEventListener('click',()=>{
     displayCambios(botonBuscar);
     displayCambios(botonBack);
     displayCambios(botonMenu);
+})
+
+botonCarritoShop.addEventListener('click',()=>{
+    seccionCarritoDesplegable.classList.toggle("contenedor-carrito-desplegado")
+})
+botonBackCarrito.addEventListener('click',()=>{
+    seccionCarritoDesplegable.classList.toggle("contenedor-carrito-desplegado")
 })
 
